072bc40a04346cf61f22ed0b6aefefa9aafa3717
wtwang1 <wtwang1@sheffield.ac.uk>
2016-10-31 14:17:56 +0000

    Made change to HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,8 +1,8 @@
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World");
-
+		system.out.println("Hello World");
+		system.out.println("Hello Friend");
 	}
 
 }
