c750a45eab356f87a09aedda5e4f91fe3db668c3
cwdellar <cwdellar1@sheffield.ac.uk>
2016-10-31 21:53:12 +0000

    Test

---
+++ SheffieldDentalCare/src/sheffieldDentalCare/Test.java
@@ -0,0 +1,9 @@
+package sheffieldDentalCare;
+
+public class Test {
+	public static void main(String[] args) {
+		System.out.println("Test Hello World");
+		System.out.println("Test Hello Friend");
+	}
+
+}
