fb9f21cd50bdf09dbfe20d104c7a0dcfc49a6996
cwdellar <cwdellar1@sheffield.ac.uk>
2016-10-31 22:41:41 +0000

    Hello All

--- SheffieldDentalCare/src/sheffieldDentalCare/HelloWorld.java
+++ SheffieldDentalCare/src/sheffieldDentalCare/HelloWorld.java
@@ -3,7 +3,8 @@ package sheffieldDentalCare;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello Ting");
+		
+		System.out.println("Hello All");
 		System.out.println("Hello World");
 		System.out.println("Hello Friend");
 	}
