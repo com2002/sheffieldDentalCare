56af5e35281784385533f7a3815a8f191b44422e
wtwang1 <wtwang1@sheffield.ac.uk>
2016-11-10 17:47:54 +0000

    Added button which will eventually link to a patients section.

--- SheffieldDentalCare/src/sheffieldDentalCare/SecretaryFrame.java
+++ SheffieldDentalCare/src/sheffieldDentalCare/SecretaryFrame.java
@@ -4,24 +4,64 @@ import java.awt.*;
 import javax.swing.*;
 
 @SuppressWarnings("serial")
-public class SecretaryFrame extends JFrame{
+public class SecretaryFrame extends JFrame implements Frame {
+	private JLabel titleLbl = new JLabel("Secretary - Main");
+	private JButton patientsBtn = new JButton("Patients");
 	
 	public SecretaryFrame() {
 		initFrame();
+		addComponents();
 	}
+	
+	public void initComponents() {};
 	
 	public void initFrame() {
 		setTitle("Sheffield Dental Care - Secretary");
 		Toolkit toolkit = Toolkit.getDefaultToolkit();
 		Dimension screenDimensions = toolkit.getScreenSize();
-		setSize(screenDimensions.width/5, screenDimensions.height/8);
-		setLocation(new Point(screenDimensions.width/4, screenDimensions.height/4));
+		setSize(screenDimensions.width/4, screenDimensions.height/4);
+		setLocationByPlatform(true);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setVisible(true);
 	}
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	
+	public void addComponents() {
+		JPanel mainPanel = new JPanel();
+		GroupLayout layout = new GroupLayout(mainPanel);
+		layout.setAutoCreateGaps(true);
+		layout.setAutoCreateContainerGaps(true);
+		mainPanel.setLayout(layout);
+		
+		// Add components to layout
+		// Position components in the horizontal
+		layout.setHorizontalGroup(
+			layout.createSequentialGroup()
+				.addGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING)
+					.addComponent(titleLbl)
+				.addGroup(layout.createSequentialGroup()
+					.addGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING)
+						.addComponent(patientsBtn)
+					)
+				)
+				)
+		);
+				
+		// Position components in the vertical
+		layout.setVerticalGroup(
+			layout.createSequentialGroup()
+				.addGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING)
+					.addComponent(titleLbl)
+				)
+				.addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
+					.addComponent(patientsBtn)
+				)
+		);
+		
+		Container contentPane = getContentPane();
+		contentPane.add(mainPanel);
 	}
-
+	
+	public static void main(String[] args) {
+		new SecretaryFrame();
+	}
 }
