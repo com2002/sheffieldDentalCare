6b3bc6f32eaf11479633d3fb785031d606d4aa60
Charlie <cwdellar1@sheffield.ac.uk>
2016-11-12 01:40:00 +0000

    Ammended checkClash now called timeClash. added checkAvailability.
    checkClash now removes last 3 chars from times if they are longer than 5
    chars long (i.e from DB).
    
    checkAvailability returns a string saying what clashes there are and who
    theyre with and what the appointment ID is of the clash. If no clashes
    it returns empty string.

--- SheffieldDentalCare/src/sheffieldDentalCare/DPCalendar.java
+++ SheffieldDentalCare/src/sheffieldDentalCare/DPCalendar.java
@@ -6,8 +6,48 @@ import java.util.Date;
 
 public class DPCalendar {
 
-	private static boolean checkClash(String start1, String end1, String start2, String end2) {
-		//convert times in strings to ints
+	public String checkAvailability(int patientID, boolean pHygienist, String date, String start, String end) throws SQLException {
+		Connection con = null;
+		Statement stmt = null;
+		String output1 = "";
+		String output2 = "";
+		try {
+			con = DriverManager.getConnection("jdbc:mysql://" + DBController.DB_Server + "/" + DBController.DB_Name, 
+					DBController.DB_User, DBController.DB_Password);
+			stmt = con.createStatement();
+			//check patient clashes
+			ResultSet rs = stmt.executeQuery("SELECT * FROM Appointments WHERE "
+					+ "patientID = " + patientID + " AND date = '" + date + "';");
+			while (rs.next()) {
+				if (timeClash(rs.getString(5),rs.getString(6), start, end)) {					
+					output1 = "Not available: clash with patient's appointment with ID " + rs.getInt(1)+ ". ";
+				}
+			}
+			//check partner clashes
+			ResultSet rt = stmt.executeQuery("SELECT * FROM Appointments WHERE "
+					+ "pHygienist = " + pHygienist + " AND date = '" + date + "';");
+			while (rt.next()) {
+				if (timeClash(rt.getString(5),rt.getString(6), start, end)) {
+					output2 = "Not available: clash with partner's appointment with ID " + rt.getInt(1)+ ". ";
+				}
+			}			
+		}
+		catch (SQLException ex){
+			ex.printStackTrace();
+		} 
+		finally {
+			if (stmt != null) stmt.close();
+			if (con != null) con.close();
+		}
+		return output1 + output2;
+	}
+	
+	private static boolean timeClash(String start1, String end1, String start2, String end2) {
+		//convert times in strings to ints of the form HH:MM
+		if (start1.length() > 5) start1 = start1.substring(0, start1.length()-3);
+		if (end1.length() > 5) end1 = end1.substring(0, end1.length()-3);
+		if (start2.length() > 5) start2 = start2.substring(0, start2.length()-3);
+		if (end1.length() > 5) end1 = end1.substring(0, end1.length()-3);
 		int s1 = Integer.parseInt((start1.replaceAll("[^\\d.]", "")));
 		int e1 = Integer.parseInt((end1.replaceAll("[^\\d.]", "")));
 		int s2 = Integer.parseInt((start2.replaceAll("[^\\d.]", "")));
@@ -71,12 +111,13 @@ public class DPCalendar {
 		String date = dateFormat.format(new Date());
 		String startTime = "18:00";
 		String endTime = "18:20";
-		System.out.println(date);
+		//System.out.println(date);
 		//System.out.println("Appointment ID: " + calendar.addAppointment(2, true, date, startTime, endTime));
-		//int id = calendar.addAppointment(1, true, date, startTime, endTime);
+		//int id = calendar.addAppointment(2, true, date, startTime, endTime);
 		//System.out.println("Appointment ID: " + id);
 		//System.out.println(calendar.deleteAppointment(id));
-		//System.out.println(checkClash("12:00","16:00","10:00","12:00"));
+		System.out.println(timeClash("17:50", "18:50","18:00","18:20"));
+		System.out.println(calendar.checkAvailability(1, true, "2016-11-12", "17:50", "18:50"));
 	}
 
 }
