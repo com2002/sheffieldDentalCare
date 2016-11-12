0ff05939b69ebd3fc7d159706f9d85bf9bdef51d
Charlie <cwdellar1@sheffield.ac.uk>
2016-11-12 21:41:03 +0000

    editted Checkout

--- SheffieldDentalCare/src/sheffieldDentalCare/Checkout.java
+++ SheffieldDentalCare/src/sheffieldDentalCare/Checkout.java
@@ -17,15 +17,17 @@ public class Checkout {
 	 * @param appointmentID
 	 * @param treatmentName
 	 * @throws SQLException
+	 * @returns number of rows updated
 	 */
-	public void addTreatmentToAppointment(int appointmentID, String treatmentName) throws SQLException {
+	public int addTreatmentToAppointment(int appointmentID, String treatmentName) throws SQLException {
 		Connection con = null;
 		Statement stmt = null;
+		int rowsAdded = 0;
 		try {
 			con = DriverManager.getConnection("jdbc:mysql://" + DBController.DB_Server + "/" + DBController.DB_Name, 
 					DBController.DB_User, DBController.DB_Password);
 			stmt = con.createStatement();
-			ResultSet rs = stmt.executeQuery("INSERT INTO TreatmentsPerformed "
+			rowsAdded = stmt.executeUpdate("INSERT INTO TreatmentsPerformed "
 					+ "VALUES(" + appointmentID + "," + treatmentName + ", 0),;");
 		}
 		catch (SQLException ex) {
@@ -35,6 +37,7 @@ public class Checkout {
 			if (stmt != null) stmt.close();
 			if (con != null) con.close();
 		}
+		return rowsAdded;
 	}
 
 	public void payAppointment(int appointmentID) throws SQLException {
