2bb0eb8adee04bad96e603edaaf6e44e3ab4d5db
Charlie <cwdellar1@sheffield.ac.uk>
2016-11-19 02:54:53 +0000

    Added CancelAppointmentFrame. also ammended PayTreatmentsFrame

---
+++ SheffieldDentalCare/src/sheffieldDentalCare/CancelAppointmentFrame.java
@@ -0,0 +1,166 @@
+package sheffieldDentalCare;
+
+import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import javax.swing.BorderFactory;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.LayoutStyle.ComponentPlacement;
+import javax.swing.ListSelectionModel;
+import javax.swing.border.EmptyBorder;
+
+@SuppressWarnings("serial")
+public class CancelAppointmentFrame extends JFrame {
+
+	private JPanel contentPane;
+	private JTable table;
+	int patientID;
+	private JButton btnCancelApp;
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					CancelAppointmentFrame frame = new CancelAppointmentFrame(1);
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 * @throws SQLException 
+	 */
+	public CancelAppointmentFrame(int patientID) throws SQLException {
+		this.patientID = patientID;
+		setTitle("Cancel Appointments by Patient. Current Patient's ID is: "+patientID);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 700, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		
+		JScrollPane futureAppsScrPane = new JScrollPane();
+		futureAppsScrPane.setBorder(BorderFactory.createTitledBorder("Patient's Future Appointments"));
+		
+		btnCancelApp = new JButton("<html>Cancel Selected <br>Appointment</html>");
+		btnCancelApp.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				///////////////////////////////////////
+				if (table.getSelectedRow() < 0) {
+					System.out.println("no row selected");
+				}
+				else {
+					int appIDCancel = (int)table.getValueAt(table.getSelectedRow(), 0);
+					System.out.println(appIDCancel);
+					int p = JOptionPane.showConfirmDialog(null, "<html>Are you sure you wish to cancel the selected appointment?</html>", "Confirmation", JOptionPane.YES_NO_OPTION);
+					if (p==0) {
+						Connection con = null;
+						Statement stmt = null;
+						try {
+						con = DriverManager.getConnection("jdbc:mysql://" + DBController.DB_Server + "/" + DBController.DB_Name, 
+								DBController.DB_User, DBController.DB_Password);
+						stmt = con.createStatement();
+						
+						stmt.executeUpdate("DELETE FROM Appointments WHERE appointmentID = "+appIDCancel+";");
+						
+						JOptionPane.showMessageDialog(null, "<html>The selected appointment was successfully cancelled.</html>", 
+								"Appointment Cancelled", JOptionPane.INFORMATION_MESSAGE);
+						drawTable();
+						}
+						catch (SQLException ex) {
+							JOptionPane.showMessageDialog(null, "Server error!", "Error", JOptionPane.ERROR_MESSAGE);
+							ex.printStackTrace();
+						}
+						finally {
+							try {
+							if (stmt != null) stmt.close();
+							if (con != null) con.close();
+							}
+							catch (SQLException ex) {
+								ex.printStackTrace();
+							}
+						}
+					}
+				}
+				//////////////////////////////////////////
+			}
+		});
+		GroupLayout gl_contentPane = new GroupLayout(contentPane);
+		gl_contentPane.setHorizontalGroup(
+			gl_contentPane.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addContainerGap()
+					.addComponent(futureAppsScrPane, GroupLayout.PREFERRED_SIZE, 445, GroupLayout.PREFERRED_SIZE)
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addComponent(btnCancelApp, GroupLayout.PREFERRED_SIZE, 192, GroupLayout.PREFERRED_SIZE)
+					.addContainerGap(21, Short.MAX_VALUE))
+		);
+		gl_contentPane.setVerticalGroup(
+			gl_contentPane.createParallelGroup(Alignment.TRAILING)
+				.addGroup(Alignment.LEADING, gl_contentPane.createSequentialGroup()
+					.addContainerGap()
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+						.addComponent(btnCancelApp, GroupLayout.PREFERRED_SIZE, 85, GroupLayout.PREFERRED_SIZE)
+						.addComponent(futureAppsScrPane, GroupLayout.DEFAULT_SIZE, 229, Short.MAX_VALUE))
+					.addContainerGap())
+		);
+		
+		table = new JTable();
+		table.setFillsViewportHeight(true);
+		table.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		table.getTableHeader().setReorderingAllowed(false);
+		futureAppsScrPane.setViewportView(table);
+		contentPane.setLayout(gl_contentPane);
+		
+		drawTable();
+	}
+	
+	private void drawTable() throws SQLException {
+		Connection con = null;
+		Statement stmt = null;
+		try {
+		con = DriverManager.getConnection("jdbc:mysql://" + DBController.DB_Server + "/" + DBController.DB_Name, 
+				DBController.DB_User, DBController.DB_Password);
+		stmt = con.createStatement();
+		/////////////////////////////
+		ResultSet rs = stmt.executeQuery("SELECT appointmentID as AppointmentID, "
+				+ "(CASE WHEN pHygienist = 1 THEN 'Hygienist' ELSE 'Dentist' END) as Partner, "
+				+ "date as Date, "
+				+ "startTime as StartTime, "
+				+ "endTime as EndTime FROM Appointments "
+				+ "WHERE patientID = "+patientID
+				+ " AND date > CURDATE() "
+				+ "AND startTime > CURTIME()"
+				+ ";");
+		
+		table.setModel(MyDbConverter.resultSetToMyTableModel(rs));
+		/////////////////////////////
+		}
+		catch (SQLException ex){
+			ex.printStackTrace();
+		}
+		finally {
+			if (stmt != null) stmt.close();
+			if (con != null) con.close();
+		}
+	}
+}
--- SheffieldDentalCare/src/sheffieldDentalCare/PayTreatmentsFrame.java
+++ SheffieldDentalCare/src/sheffieldDentalCare/PayTreatmentsFrame.java
@@ -10,6 +10,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
+import javax.swing.BorderFactory;
 import javax.swing.GroupLayout;
 import javax.swing.GroupLayout.Alignment;
 import javax.swing.JButton;
@@ -21,6 +22,8 @@ import javax.swing.JTable;
 import javax.swing.LayoutStyle.ComponentPlacement;
 import javax.swing.ListSelectionModel;
 
+
+@SuppressWarnings("serial")
 public class PayTreatmentsFrame extends JFrame {
 	private JTable appsTable;
 	public int patientID;
@@ -33,7 +36,8 @@ public class PayTreatmentsFrame extends JFrame {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					PayTreatmentsFrame frame = new PayTreatmentsFrame(1);
+					@SuppressWarnings("unused")
+					PayTreatmentsFrame frame = new PayTreatmentsFrame(2);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -47,7 +51,7 @@ public class PayTreatmentsFrame extends JFrame {
 	public PayTreatmentsFrame(int patientID) {
 		this.patientID = patientID;
 			
-		setTitle("View/Pay Appointments and Treatments by Patient");
+		setTitle("View/Pay Appointments and Treatments by Patient. Currently viewing patientID: "+patientID);
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		setBounds(100, 100, 1010, 602);
 		setVisible(true);
@@ -55,15 +59,21 @@ public class PayTreatmentsFrame extends JFrame {
 		JButton btnLoadData = new JButton("<html>Load Patient's <br>Appointments</html>");
 		btnLoadData.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				drawAppsTable();			
+				try {
+					drawAppsTable();
+				} catch (SQLException e1) {
+					JOptionPane.showMessageDialog(null, "Server error!", "Error", JOptionPane.ERROR_MESSAGE);
+					e1.printStackTrace();
+				}			
 			}
 
 			
 		});
 		
-		JScrollPane scrollPane = new JScrollPane();
-		
-		JScrollPane scrollPane_1 = new JScrollPane();
+		JScrollPane appsTableScrPane = new JScrollPane();
+		appsTableScrPane.setBorder(BorderFactory.createTitledBorder("Patient's Appointments"));
+		JScrollPane tmentsTableScrPane = new JScrollPane();
+		tmentsTableScrPane.setBorder(BorderFactory.createTitledBorder("Treatments Performed"));
 		
 		JLabel lblPatientid = new JLabel("PatientID: " + patientID);
 		
@@ -79,7 +89,12 @@ public class PayTreatmentsFrame extends JFrame {
 					appID = (int)appsTable.getValueAt(appsTable.getSelectedRow(), 0);
 					lblAppointmentid.setText("AppointmentID: "+Integer.toString(appID));
 					System.out.println(appID);
-					drawTreatmentsTable();
+					try {
+						drawTreatmentsTable();
+					} catch (SQLException e) {
+						JOptionPane.showMessageDialog(null, "Server error!", "Error", JOptionPane.ERROR_MESSAGE);
+						e.printStackTrace();
+					}
 				}
 			}
 		});
@@ -122,7 +137,6 @@ public class PayTreatmentsFrame extends JFrame {
 		JButton btnPaySelectedTreatment = new JButton("Pay selected Treatment");
 		btnPaySelectedTreatment.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
-				//TODO
 				//Get treatment name and appointmentID
 				//Change paid to true for this treatment performed.
 				if (tmentTable.getSelectedRow() < 0) {
@@ -141,10 +155,12 @@ public class PayTreatmentsFrame extends JFrame {
 							Checkout checkout = new Checkout();
 							try {						
 								checkout.payTreatment(appID,tmentName);
-								drawTreatmentsTable();
-								drawAppsTable();
+								
 								JOptionPane.showMessageDialog(null, "<html>The treatment selected has now been paid.</html>", 
 										"Appointment Paid", JOptionPane.INFORMATION_MESSAGE);
+								
+								drawTreatmentsTable();
+								drawAppsTable();
 							}
 							catch (SQLException e1) {
 								JOptionPane.showMessageDialog(null, "Server error!", "Error", JOptionPane.ERROR_MESSAGE);
@@ -164,64 +180,59 @@ public class PayTreatmentsFrame extends JFrame {
 				.addGroup(groupLayout.createSequentialGroup()
 					.addContainerGap()
 					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
-						.addGroup(groupLayout.createSequentialGroup()
-							.addComponent(scrollPane, GroupLayout.PREFERRED_SIZE, 655, GroupLayout.PREFERRED_SIZE)
-							.addPreferredGap(ComponentPlacement.RELATED)
-							.addGroup(groupLayout.createParallelGroup(Alignment.LEADING, false)
-								.addComponent(btnLoadData, GroupLayout.DEFAULT_SIZE, 190, Short.MAX_VALUE)
-								.addComponent(lblPatientid, GroupLayout.PREFERRED_SIZE, 78, GroupLayout.PREFERRED_SIZE)
-								.addComponent(btnPayApp, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
-						.addGroup(groupLayout.createSequentialGroup()
-							.addComponent(scrollPane_1, GroupLayout.PREFERRED_SIZE, 655, GroupLayout.PREFERRED_SIZE)
-							.addPreferredGap(ComponentPlacement.UNRELATED)
-							.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
-								.addComponent(btnPaySelectedTreatment)
-								.addComponent(btnLoadSelApp)
-								.addComponent(lblAppointmentid, GroupLayout.PREFERRED_SIZE, 293, GroupLayout.PREFERRED_SIZE))))
-					.addContainerGap(26, Short.MAX_VALUE))
+						.addComponent(tmentsTableScrPane, GroupLayout.PREFERRED_SIZE, 655, GroupLayout.PREFERRED_SIZE)
+						.addComponent(appsTableScrPane, GroupLayout.PREFERRED_SIZE, 655, GroupLayout.PREFERRED_SIZE))
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addGroup(groupLayout.createParallelGroup(Alignment.TRAILING)
+						.addComponent(lblAppointmentid, GroupLayout.DEFAULT_SIZE, 313, Short.MAX_VALUE)
+						.addComponent(lblPatientid, Alignment.LEADING, GroupLayout.PREFERRED_SIZE, 78, GroupLayout.PREFERRED_SIZE)
+						.addComponent(btnLoadData, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, 313, Short.MAX_VALUE)
+						.addComponent(btnLoadSelApp, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, 313, Short.MAX_VALUE)
+						.addComponent(btnPaySelectedTreatment, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, 313, Short.MAX_VALUE)
+						.addComponent(btnPayApp, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, 313, Short.MAX_VALUE))
+					.addContainerGap())
 		);
 		groupLayout.setVerticalGroup(
 			groupLayout.createParallelGroup(Alignment.LEADING)
 				.addGroup(groupLayout.createSequentialGroup()
-					.addContainerGap()
+					.addContainerGap(25, Short.MAX_VALUE)
 					.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
-						.addComponent(scrollPane, GroupLayout.PREFERRED_SIZE, 272, GroupLayout.PREFERRED_SIZE)
+						.addComponent(appsTableScrPane, GroupLayout.PREFERRED_SIZE, 251, GroupLayout.PREFERRED_SIZE)
 						.addGroup(groupLayout.createSequentialGroup()
-							.addComponent(lblPatientid, GroupLayout.PREFERRED_SIZE, 34, GroupLayout.PREFERRED_SIZE)
-							.addGap(83)
+							.addComponent(lblPatientid, GroupLayout.PREFERRED_SIZE, 24, GroupLayout.PREFERRED_SIZE)
+							.addPreferredGap(ComponentPlacement.RELATED)
+							.addComponent(btnLoadData, GroupLayout.PREFERRED_SIZE, 65, GroupLayout.PREFERRED_SIZE)
+							.addGap(7)
 							.addComponent(btnPayApp)))
-					.addPreferredGap(ComponentPlacement.RELATED, 8, Short.MAX_VALUE)
+					.addPreferredGap(ComponentPlacement.RELATED, 35, Short.MAX_VALUE)
 					.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
-						.addComponent(scrollPane_1, GroupLayout.PREFERRED_SIZE, 272, GroupLayout.PREFERRED_SIZE)
+						.addComponent(tmentsTableScrPane, GroupLayout.PREFERRED_SIZE, 241, GroupLayout.PREFERRED_SIZE)
 						.addGroup(groupLayout.createSequentialGroup()
 							.addComponent(lblAppointmentid, GroupLayout.PREFERRED_SIZE, 24, GroupLayout.PREFERRED_SIZE)
-							.addGap(5)
-							.addComponent(btnLoadSelApp)
-							.addGap(10)
-							.addComponent(btnPaySelectedTreatment))))
-				.addGroup(groupLayout.createSequentialGroup()
-					.addGap(52)
-					.addComponent(btnLoadData, GroupLayout.PREFERRED_SIZE, 65, GroupLayout.PREFERRED_SIZE)
-					.addContainerGap(446, Short.MAX_VALUE))
+							.addGap(4)
+							.addComponent(btnLoadSelApp, GroupLayout.PREFERRED_SIZE, 56, GroupLayout.PREFERRED_SIZE)
+							.addGap(3)
+							.addComponent(btnPaySelectedTreatment, GroupLayout.PREFERRED_SIZE, 62, GroupLayout.PREFERRED_SIZE)))
+					.addContainerGap(GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
 		);
 		
 		tmentTable = new JTable();
 		tmentTable.getTableHeader().setReorderingAllowed(false);
 		tmentTable.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-		scrollPane_1.setViewportView(tmentTable);
+		tmentsTableScrPane.setViewportView(tmentTable);
 		tmentTable.setFillsViewportHeight(true);
 		
 		appsTable = new JTable();
 		appsTable.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
 		appsTable.getTableHeader().setReorderingAllowed(false);
-		scrollPane.setViewportView(appsTable);
+		appsTableScrPane.setViewportView(appsTable);
 		
 		appsTable.setFillsViewportHeight(true);
 		
 		getContentPane().setLayout(groupLayout);
 	}
 	
-	private void drawAppsTable() {
+	private void drawAppsTable() throws SQLException {
 		Connection con = null;
 		Statement stmt = null;
 		try {
@@ -236,15 +247,18 @@ public class PayTreatmentsFrame extends JFrame {
 		
 		appsTable.setModel(MyDbConverter.resultSetToMyTableModel(rs));
 		
-		if (stmt != null) stmt.close();
-		if (con != null) con.close();
+		
 		}
 		catch (SQLException ex){
 			ex.printStackTrace();
-		}	
+		}
+		finally {
+			if (stmt != null) stmt.close();
+			if (con != null) con.close();
+		}
 	}
 	
-	private void drawTreatmentsTable() {
+	private void drawTreatmentsTable() throws SQLException {
 		Connection con = null;
 		Statement stmt = null;
 		try {
@@ -256,11 +270,14 @@ public class PayTreatmentsFrame extends JFrame {
 				+ "(SELECT * from TreatmentsPerformed Natural JOIN Treatments WHERE appointmentID = "+appID+") as als;");
 		
 		tmentTable.setModel(MyDbConverter.resultSetToMyTableModel(rs));
-		if (stmt != null) stmt.close();
-		if (con != null) con.close();
+		
 		}
 		catch (SQLException ex){
 			ex.printStackTrace();
+		}
+		finally {
+			if (stmt != null) stmt.close();
+			if (con != null) con.close();
 		}
 	}
 }
